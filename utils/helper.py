48a8fb2bc6ebdf0d6b54cd2682a4165ef8f59542
Jack Snyder <jacksnyde@gmail.com>
2024-07-19 13:18:19 -0600

    Basic Images and Text Working

--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,5 @@
 from constants import stopCode
+from PIL import Image
 
 def binListToInt(binList):
     binString = ''.join(binList)
@@ -10,3 +11,19 @@ def binStringToChar(binString):
 
 def checkForStopCode(str1):
     return stopCode in str1
+
+def indexInHidden(index: int, originalImage: Image, hiddenImage: Image):
+    originalDimensions = originalImage.size
+    hiddenDimensions = hiddenImage.size
+    yIndex = index // originalDimensions[0]
+    xIndex = index % originalDimensions[0]
+    return xIndex < hiddenDimensions[0] and yIndex < hiddenDimensions[1]
+
+def originalIndexToHiddenIndex(originalIndex: int, originalImage: Image, hiddenImage: Image):
+    originalDimensions = originalImage.size
+    hiddenDimensions = hiddenImage.size
+    
+    originalY = originalIndex // originalDimensions[0]
+    orignalX = originalIndex % originalDimensions[0]
+    
+    return orignalX + originalY * hiddenDimensions[0]
